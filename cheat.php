348561cd228c093fdef9ec532d4f2305103ef576
Christoph Niedermoser <chris@niedermoser.eu>
2019-09-19 08:23:59 +0200

    Create cheat.php

---
+++ cheat.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Iterate Entities of Reference Field]
+ */
+/** @var \Drupal\Core\Field\EntityReferenceFieldItemList $entity */
+$field = $entity->get('field_references');
+
+/** @var \Drupal\node\Entity\Node $state */
+foreach ($field->referencedEntities() as $entity) {
+  $entity->doSomething();
+}
